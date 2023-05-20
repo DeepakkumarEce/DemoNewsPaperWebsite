d7d7c67acb225f2dc6f32b94ac73faff5207209a
DeepakkumarEce <86106625+DeepakkumarEce@users.noreply.github.com>
2023-05-20 14:08:46 +0530

    optionform added

---
+++ optionform.js
@@ -0,0 +1,31 @@
+function openthehindu(){
+        var th_newspaperName = document.getElementById("th_newspaperName").value;
+        var th_date = document.getElementById("th_date").value;
+        var th_language = document.getElementById("th_language").value;
+        alert (th_newspaperName + th_date + th_language);
+}
+
+function openthenewyorktimes(){
+          var nt_newspaperName = document.getElementById("nt_newspaperName").value;
+          var nt_date = document.getElementById("nt_date").value;
+          var nt_language = document.getElementById("nt_language").value;
+          alert (nt_newspaperName + nt_date + nt_language);
+}
+function openhindustantimes(){
+          var ht_newspaperName = document.getElementById("ht_newspaperName").value;
+          var ht_date = document.getElementById("ht_date").value;
+          var ht_language = document.getElementById("ht_language").value;
+          alert (ht_newspaperName + ht_date + ht_language); 
+}
+function opentimesofindia(){
+          var toi_newspaperName = document.getElementById("toi_newspaperName").value;
+          var toi_date = document.getElementById("toi_date").value;
+          var toi_language = document.getElementById("toi_language").value;
+          alert (toi_newspaperName + toi_date + toi_language);
+}
+function openecotimes(){
+          var et_newspaperName = document.getElementById("et_newspaperName").value;
+          var et_date = document.getElementById("et_date").value;
+          var et_language = document.getElementById("et_language").value;
+          alert (et_newspaperName + et_date + et_language);
+}
